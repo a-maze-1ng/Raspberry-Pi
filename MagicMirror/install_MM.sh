e59939da40bbcfa521fdc7cdf0d9a5ad4020a6e6
a-maze-1ng <maze-github@polack.email>
2022-05-31 21:26:44 +0200

    update

--- MagicMirror/install_MM.sh
+++ MagicMirror/install_MM.sh
@@ -25,16 +25,13 @@ cp config/config.js.sample config/config.js
 #Create startup file
 ####################################################
 cd /home/pi
-echo "#!/bin/bash" > start_mm.sh
-echo "sleep 30s" >> start_mm.sh
-echo "cd /home/pi/MagicMirror" >> start_mm.sh
-echo "npm run start" >> start_mm.sh
+wget https://raw.githubusercontent.com/a-maze-1ng/Raspberry-Pi/main/MagicMirror/config/start_mm.sh
 sudo chmod +x start_mm.sh
 ####################################################
 #Create cron job for automatic start
 ####################################################
 cd /home/pi
-echo "@reboot pi /home/pi/start_mm.sh" > startMM
+wget https://raw.githubusercontent.com/a-maze-1ng/Raspberry-Pi/main/MagicMirror/config/startMM
 sudo chown root:root startMM
 sudo mv startMM /etc/cron.d/startMM #careful cron.d does not like .,_- characters!
 ####################################################
@@ -44,14 +41,17 @@ cd /home/pi
 mkdir dummy
 cd /home/pi/dummy
 wget https://raw.githubusercontent.com/a-maze-1ng/Raspberry-Pi/main/MagicMirror/config/config.js
+wget https://raw.githubusercontent.com/a-maze-1ng/Raspberry-Pi/main/MagicMirror/config/lightdm.conf
 ####################################################
-#Backup config file
+#Backup
 ####################################################
 sudo cp /home/pi/MagicMirror/config/config.js /home/pi/MagicMirror/config/config.js_backup
+sudo cp /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf_backup
 ####################################################
-#Copy config file to MagicMirror directory
+#Copy config files
 ####################################################
 sudo mv /home/pi/dummy/config.js /home/pi/MagicMirror/config/config.js
+sudo mv /home/pi/dummy/lightdm.conf /etc/lightdm/lightdm.conf
 ####################################################
 #Cleanup
 ####################################################
