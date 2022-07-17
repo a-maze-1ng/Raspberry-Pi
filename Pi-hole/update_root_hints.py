8c4bc6e91d8308f69a00736598a2f22b8b402926
a-maze-1ng <105156333+a-maze-1ng@users.noreply.github.com>
2022-07-17 11:54:26 +0200

    Update update_root_hints.py

--- Pi-hole/update_root_hints.py
+++ Pi-hole/update_root_hints.py
@@ -5,7 +5,7 @@ os.system('echo "sleep for 60s"')
 time.sleep(60)
 os.system('sudo rm /var/lib/unbound/root.hints')
 os.system('echo "delete done"')
-os.system('sudo wget https://www.internic.net/domain/named.root -qO- | sudo tee$
+os.system('wget https://www.internic.net/domain/named.root -qO- | sudo tee /var/lib/unbound/root.hints')
 os.system('echo "hints download done"')
 os.system('sudo service unbound restart')
 os.system('echo "unbound service restarted"')
